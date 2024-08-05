2c3aa5448de069a5636a391f3ed0d1e17760f631
Randall Le <randall3@gatech.edu>
2024-08-05 15:21:27 -0700

    Initial Commit

---
+++ iPantry/iPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPantryApp.swift
+//  iPantry
+//
+//  Created by Randall Le on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
