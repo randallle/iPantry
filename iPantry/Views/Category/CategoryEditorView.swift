11ec0b298efc5f94996a6fa6e6207a18053abcf9
Randall Le <randallnguyenle@gmail.com>
2024-08-27 16:14:50 -0700

    #31: started on CategoryEditorView

---
+++ iPantry/Views/Category/CategoryEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryEditorView.swift
+//  iPantry
+//
+//  Created by Randall Le on 8/27/24.
+//
+
+import SwiftUI
+
+struct CategoryEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryEditorView()
+}
