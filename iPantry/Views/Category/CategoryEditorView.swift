d2d002ef766e4404816748c9478f258e86904ed3
Randall Le <randallnguyenle@gmail.com>
2024-08-27 16:29:14 -0700

    #31: do not show Other category

--- iPantry/Views/Category/CategoryEditorView.swift
+++ iPantry/Views/Category/CategoryEditorView.swift
@@ -5,14 +5,57 @@
 //  Created by Randall Le on 8/27/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct CategoryEditorView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @Query(sort: [SortDescriptor(\Category.name, order: .forward)]) private var categories: [Category]
+    
+    var otherCategory: Category {
+        guard let other = categories.first(where: { $0.name == "Other" }) else {
+            fatalError("Could not retrieve category \"Other\".")
+        }
+        return other
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(categories, id: \.self) { category in
+                        if category.name != "Other" {
+                            Text(category.name)
+                        }
+                    }
+                } header: {
+                    Text("Categories")
+                } footer: {
+                    Text("Swipe to delete or rename a category")
+                }
+            }
+            .navigationTitle("Edit categories")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+            
+            
+        }
     }
 }
 
 #Preview {
-    CategoryEditorView()
+    let preview = Preview(Category.self)
+    preview.addSamples(Category.sampleCategories)
+    return NavigationStack {
+        CategoryEditorView()
+            .modelContainer(preview.container)
+    }
 }
