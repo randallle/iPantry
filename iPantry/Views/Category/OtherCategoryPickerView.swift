7aaa8e0b0e5c2e1c5f7ca15e0443bf81938703ab
Randall Le <randallnguyenle@gmail.com>
2024-08-27 21:47:56 -0700

    #31: category picker

--- iPantry/Views/Category/OtherCategoryPickerView.swift
+++ iPantry/Views/Category/OtherCategoryPickerView.swift
@@ -14,11 +14,40 @@ struct OtherCategoryPickerView: View {
     
     @Query(sort: [SortDescriptor(\Category.name, order: .forward)]) private var categories: [Category]
     
+    @State private var selectedCategory: Category?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Picker("Categories", selection: $selectedCategory) {
+                ForEach(categories, id: \.self) { category in
+                    Text(category.name).tag(category as Category?)
+                }
+            }
+            .pickerStyle(.inline)
+            
+            Section {
+                List {
+                    Button("Create Category") {}
+                    Button("Manage Categories") {}
+                }
+            } header: {
+                Text("More options")
+            }
+        }
+        .navigationTitle("Select Category")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    OtherCategoryPickerView()
+    let preview = Preview(Category.self, Item.self)
+    Task {
+        await preview.addSamplesAsync(Category.sampleCategories)
+        await preview.addSamplesAsync(Item.sampleItems)
+    }
+    
+    return NavigationStack {
+        OtherCategoryPickerView()
+            .modelContainer(preview.container)
+    }
 }
